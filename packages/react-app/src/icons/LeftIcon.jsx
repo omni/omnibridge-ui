f0b550541dc161d89c9d8141db6103e86ead7419
dan13ram <dan13ram@gmail.com>
2020-09-17 03:15:06 +0530

    history pagination

---
+++ packages/react-app/src/icons/LeftIcon.jsx
@@ -0,0 +1,15 @@
+import { createIcon } from '@chakra-ui/icon';
+import * as React from 'react';
+
+export const LeftIcon = createIcon({
+  displayName: 'LeftIcon',
+  path: (
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M0.707107 11.2929C0.316582 11.6834 0.316582 12.3166 0.707107 12.7071L10.6066 22.6066C10.9971 22.9971 11.6303 22.9971 12.0208 22.6066C12.4113 22.2161 12.4113 21.5829 12.0208 21.1924L2.82843 12L12.0208 2.80761C12.4113 2.41709 12.4113 1.78392 12.0208 1.3934C11.6303 1.00287 10.9971 1.00287 10.6066 1.3934L0.707107 11.2929Z"
+      fill="currentColor"
+    />
+  ),
+  viewBox: '0 0 23 24',
+});
