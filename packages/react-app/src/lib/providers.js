98872921a2fd0adab126918bef3d79b52cde552a
Sundeep Charan Ramkumar <webdevdesign@sundeepcharan.com>
2021-04-29 19:52:39 +0530

    bugfix: devise a better way for timing out longer ping urls

--- packages/react-app/src/lib/providers.js
+++ packages/react-app/src/lib/providers.js
@@ -19,31 +19,43 @@ const RPC_URL = {
   100: XDAI_RPC_URL,
 };
 
+const NETWORK_TIMEOUT = 1000;
+
 const memoized = memoize(
   url => new ethers.providers.StaticJsonRpcProvider(url),
 );
 
+const checkRPCHealth = async url => {
+  if (!url) return null;
+  const tempProvider = memoized(url);
+  if (!tempProvider) return null;
+  try {
+    await Promise.race([
+      // eslint-disable-next-line no-underscore-dangle
+      tempProvider._networkPromise,
+      setTimeout(
+        () => Promise.reject(new Error('Network timeout')).catch(() => null),
+        NETWORK_TIMEOUT,
+      ),
+    ]);
+    return tempProvider;
+  } catch (err) {
+    logError({ providerSetError: err.message });
+    return null;
+  }
+};
+
 export const getEthersProvider = async chainId => {
+  const sessionHealthyURL = `HEALTHY_RPC_URL-${chainId}`;
   const localRPCUrl = window.localStorage.getItem(RPC_URL[chainId]);
-  const rpcURLs = localRPCUrl
-    ? [localRPCUrl].concat(getRPCUrl(chainId, true))
-    : getRPCUrl(chainId, true);
-  const provider = (
-    await Promise.all(
-      rpcURLs.map(async url => {
-        const tempProvider = memoized(url);
-        if (!tempProvider) return tempProvider;
-        try {
-          // eslint-disable-next-line no-underscore-dangle
-          await tempProvider._networkPromise;
-          return tempProvider;
-        } catch (err) {
-          logError({ providerSetError: err.message });
-          return null;
-        }
-      }),
-    )
-  ).filter(p => !!p)[0];
+  const currentRPCUrls = getRPCUrl(chainId, true);
+  const rpcURLs =
+    localRPCUrl?.length > 0 ? [localRPCUrl, ...currentRPCUrls] : currentRPCUrls;
+
+  const provider =
+    (await checkRPCHealth(sessionStorage.getItem(sessionHealthyURL))) ||
+    (await Promise.all(rpcURLs.map(checkRPCHealth))).filter(p => !!p)[0];
+  sessionStorage.setItem(sessionHealthyURL, provider.connection.url);
   return provider || null;
 };
 
