5d29117957c63519429809ce9a6bd552abcf6bd2
dan13ram <dan13ram@gmail.com>
2021-07-03 23:28:10 +0530

    Indicate transfer claiming by a spinner

--- packages/dapp/src/components/modals/BridgeLoadingModal.jsx
+++ packages/dapp/src/components/modals/BridgeLoadingModal.jsx
@@ -1,3 +1,4 @@
+import { CheckIcon } from '@chakra-ui/icons';
 import {
   Flex,
   Image,
@@ -6,9 +7,9 @@ import {
   ModalBody,
   ModalContent,
   ModalOverlay,
+  Spinner,
   Text,
 } from '@chakra-ui/react';
-import BlueTickImage from 'assets/blue-tick.svg';
 import LoadingImage from 'assets/loading.svg';
 import { ProgressRing } from 'components/common/ProgressRing';
 import { ClaimTokensModal } from 'components/modals/ClaimTokensModal';
@@ -34,107 +35,122 @@ const BridgeLoader = ({
   confirmations,
   totalConfirms,
   getMonitorUrl,
-  fromToken,
-}) => (
-  <Modal
-    isOpen={loading}
-    closeOnEsc={false}
-    closeOnOverlayClick={false}
-    isCentered
-  >
-    <ModalOverlay background="modalBG">
-      <>
-        {loadingText ? (
-          <ModalContent
-            boxShadow="0px 1rem 2rem #617492"
-            borderRadius={{ base: '1rem', md: 'full' }}
-            mx={{ base: 12, lg: 0 }}
-            maxW={{ base: '20rem', md: '25rem' }}
-          >
-            <ModalBody p={4}>
-              <Flex
-                align={{ base: 'center', md: 'stretch' }}
-                direction={{ base: 'column', md: 'row' }}
-              >
+  chainId,
+}) => {
+  const showConfirmations = confirmations < totalConfirms;
+  const displayConfirms = showConfirmations ? confirmations : totalConfirms;
+
+  return (
+    <Modal
+      isOpen={loading}
+      closeOnEsc={false}
+      closeOnOverlayClick={false}
+      isCentered
+    >
+      <ModalOverlay background="modalBG">
+        <>
+          {loadingText ? (
+            <ModalContent
+              boxShadow="0px 1rem 2rem #617492"
+              borderRadius={{ base: '1rem', md: 'full' }}
+              mx={{ base: 12, lg: 0 }}
+              maxW={{ base: '20rem', md: '25rem' }}
+            >
+              <ModalBody px={4} py={8}>
                 <Flex
-                  height="5rem"
-                  width="5rem"
-                  align="center"
-                  justify="center"
-                  border="5px solid #eef4fd"
-                  borderRadius="50%"
-                  mr={4}
-                  position="relative"
+                  align={{ base: 'center', md: 'stretch' }}
+                  direction={{ base: 'column', md: 'row' }}
                 >
-                  {confirmations < totalConfirms ? (
-                    <Text>
-                      {confirmations}/{totalConfirms}
-                    </Text>
-                  ) : (
-                    <Image src={BlueTickImage} />
-                  )}
                   <Flex
-                    position="absolute"
-                    justify="center"
+                    h="3.25rem"
+                    w="3.25rem"
                     align="center"
-                    color="blue.500"
+                    justify="center"
+                    border="5px solid #eef4fd"
+                    borderRadius="50%"
+                    ml={2}
+                    mr={4}
+                    mb={{ base: 2, md: 0 }}
+                    position="relative"
                   >
-                    <ProgressRing
-                      radius={47.5}
-                      stroke={5}
-                      progress={
-                        confirmations < totalConfirms
-                          ? confirmations
-                          : totalConfirms
-                      }
-                      totalProgress={totalConfirms}
-                    />
+                    {showConfirmations ? (
+                      <>
+                        <Text fontSize="sm">
+                          {displayConfirms}/{totalConfirms}
+                        </Text>
+                        <Flex
+                          position="absolute"
+                          justify="center"
+                          align="center"
+                          color="blue.500"
+                        >
+                          <ProgressRing
+                            radius={33.5}
+                            stroke={5}
+                            progress={displayConfirms}
+                            totalProgress={totalConfirms}
+                          />
+                        </Flex>
+                      </>
+                    ) : (
+                      <>
+                        <CheckIcon color="blue.500" boxSize="0.85rem" />
+                        <Spinner
+                          position="absolute"
+                          color="blue.500"
+                          thickness="5px"
+                          w="3.25rem"
+                          h="3.25rem"
+                          speed="0.75s"
+                        />
+                      </>
+                    )}
+                  </Flex>
+                  <Flex
+                    flex={1}
+                    direction="column"
+                    align={{ base: 'center', md: 'flex-start' }}
+                    justify="center"
+                    mt={{ base: 2, md: 0 }}
+                  >
+                    <Text textAlign="center">
+                      {`${loadingText || 'Waiting for Block Confirmations'}...`}
+                    </Text>
+                    <Text color="grey" textAlign="center">
+                      {'Monitor at ALM '}
+                      <Link
+                        href={getMonitorUrl(chainId, txHash)}
+                        rel="noreferrer noopener"
+                        target="_blank"
+                        color="blue.500"
+                      >
+                        {getTransactionString(txHash)}
+                      </Link>
+                    </Text>
                   </Flex>
                 </Flex>
-                <Flex
-                  flex={1}
-                  direction="column"
-                  align="center"
-                  justify="center"
-                  mt={{ base: 2, md: 0 }}
-                >
-                  <Text width="100%">
-                    {`${loadingText || 'Waiting for Block Confirmations'}...`}
-                  </Text>
-                  <Text width="100%" color="grey">
-                    {'Monitor at ALM '}
-                    <Link
-                      href={getMonitorUrl(fromToken.chainId, txHash)}
-                      rel="noreferrer noopener"
-                      target="_blank"
-                      color="blue.500"
-                    >
-                      {getTransactionString(txHash)}
-                    </Link>
-                  </Text>
-                </Flex>
+              </ModalBody>
+            </ModalContent>
+          ) : (
+            <ModalContent background="none" boxShadow="none" borderRadius="0">
+              <Flex direction="column" align="center" justify="center">
+                <Image src={LoadingImage} mb={4} />
+                <Text color="white" fontWeight="bold">
+                  Loading ...
+                </Text>
               </Flex>
-            </ModalBody>
-          </ModalContent>
-        ) : (
-          <ModalContent background="none" boxShadow="none" borderRadius="0">
-            <Flex direction="column" align="center" justify="center">
-              <Image src={LoadingImage} mb={4} />
-              <Text color="white" fontWeight="bold">
-                Loading ...
-              </Text>
-            </Flex>
-          </ModalContent>
-        )}
-      </>
-    </ModalOverlay>
-  </Modal>
-);
+            </ModalContent>
+          )}
+        </>
+      </ModalOverlay>
+    </Modal>
+  );
+};
 
 export const BridgeLoadingModal = () => {
   const { providerChainId: chainId } = useWeb3Context();
   const { getMonitorUrl, homeChainId, foreignChainId } = useBridgeDirection();
-  const { loading, fromToken, txHash, totalConfirms } = useBridgeContext();
+  const { loading, txHash, totalConfirms } = useBridgeContext();
   const [message, setMessage] = useState();
   const {
     loadingText,
@@ -173,7 +189,7 @@ export const BridgeLoadingModal = () => {
       loading={loading}
       confirmations={confirmations}
       totalConfirms={totalConfirms}
-      fromToken={fromToken}
+      chainId={chainId}
       getMonitorUrl={getMonitorUrl}
       txHash={txHash}
     />
