deef0284195de94127968166b2fc3d11c1a1eb27
Sundeep Charan Ramkumar <webdevdesign@sundeepcharan.com>
2021-09-02 19:48:24 +0530

    chore: add env variable to github actions

--- packages/dapp/src/components/common/CoinzillaBannerAd.jsx
+++ packages/dapp/src/components/common/CoinzillaBannerAd.jsx
@@ -1,21 +1,22 @@
 import { Box } from '@chakra-ui/react';
+import { COINZILLA_API_KEY } from 'lib/constants';
 import React, { useEffect } from 'react';
 
 export const CoinzillaBannerAd = props => {
   useEffect(() => {
     window.coinzilla_display = window.coinzilla_display || [];
     window.coinzilla_display.push({
-      zone: process.env.REACT_APP_COINZILLA_API_KEY,
+      zone: COINZILLA_API_KEY,
       width: window.screen.availWidth >= 728 ? '728' : '320',
       height: window.screen.availWidth >= 728 ? '90' : '100',
     });
   }, []);
 
-  if (!process.env.REACT_APP_COINZILLA_API_KEY) return null;
+  if (!COINZILLA_API_KEY) return null;
   return (
     <Box
       className="coinzilla"
-      data-zone={`C-${process.env.REACT_APP_COINZILLA_API_KEY}`}
+      data-zone={`C-${COINZILLA_API_KEY}`}
       my="30px"
       mx="auto"
       {...props}
