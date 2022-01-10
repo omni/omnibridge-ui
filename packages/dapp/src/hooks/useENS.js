125984575131d4801edeca0f926b4434bfb35756
dan13ram <dan13ram@gmail.com>
2022-01-10 16:14:32 +0530

    fix: padding for ens name

--- packages/dapp/src/hooks/useENS.js
+++ packages/dapp/src/hooks/useENS.js
@@ -1,16 +1,21 @@
 import { ethers } from 'ethers';
+import { logError } from 'lib/helpers';
 import { getEthersProvider } from 'lib/providers';
 import { useEffect, useState } from 'react';
 
 export function useENS(address) {
-  const [ensName, setENSName] = useState();
+  const [ensName, setENSName] = useState('');
 
   useEffect(() => {
     async function resolveENS() {
-      if (ethers.utils.isAddress(address)) {
-        const provider = await getEthersProvider(1);
-        const name = await provider.lookupAddress(address);
-        if (name) setENSName(name);
+      try {
+        if (ethers.utils.isAddress(address)) {
+          const provider = await getEthersProvider(1);
+          const name = await provider.lookupAddress(address);
+          if (name) setENSName(name);
+        }
+      } catch (err) {
+        logError({ ensError: err });
       }
     }
     resolveENS();
