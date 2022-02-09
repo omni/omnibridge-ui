6f26349c6802204bdb58aee3ab2f28b6cae9c1d0
dan13ram <dan13ram@gmail.com>
2022-02-09 18:24:58 +0530

    feat: disable gc stable tokens to bsc

--- packages/dapp/src/components/warnings/BinancePeggedAssetWarning.jsx
+++ packages/dapp/src/components/warnings/BinancePeggedAssetWarning.jsx
@@ -1,78 +1,92 @@
 import { Alert, AlertIcon, Flex, Link, Text } from '@chakra-ui/react';
 import { useBridgeDirection } from 'hooks/useBridgeDirection';
 import { BSC_XDAI_BRIDGE } from 'lib/constants';
+import { getNetworkName } from 'lib/helpers';
 import React from 'react';
 
-const binancePeggedAssets = {
-  ['0xe91d153e0b41518a2ce8dd3d7944fa863463a97d'.toLowerCase()]: {
-    fromSymbol: 'WXDAI',
-    toSymbol: 'DAI',
-    componentFinanceLink:
-      'https://xdai.component.finance/?tab=swap&token0=1&token1=3',
-  },
-  ['0xDDAfbb505ad214D7b80b1f830fcCc89B60fb7A83'.toLowerCase()]: {
-    fromSymbol: 'USDC',
-    toSymbol: 'USDC',
-    componentFinanceLink:
-      'https://xdai.component.finance/?tab=swap&token0=2&token1=4',
-  },
-};
+const GC_STABLE_ASSETS = [
+  '0xDDAfbb505ad214D7b80b1f830fcCc89B60fb7A83'.toLowerCase(),
+  '0xe91d153e0b41518a2ce8dd3d7944fa863463a97d'.toLowerCase(),
+  '0x44fA8E6f47987339850636F88629646662444217'.toLowerCase(),
+  '0x4ECaBa5870353805a9F068101A40E0f32ed605C6'.toLowerCase(),
+];
 
-const getBinancePeggedAsset = tokenAddress =>
-  binancePeggedAssets[tokenAddress.toLowerCase()] ||
-  binancePeggedAssets[
-    '0xe91d153e0b41518a2ce8dd3d7944fa863463a97d'.toLowerCase()
-  ];
+const DAI_TOKEN = '0x44fA8E6f47987339850636F88629646662444217'.toLowerCase();
 
-const getComponentFinanceLink = tokenAddress =>
-  getBinancePeggedAsset(tokenAddress).componentFinanceLink;
-
-const getBinancePeggedAssetSymbol = tokenAddress =>
-  getBinancePeggedAsset(tokenAddress).toSymbol;
-
-const isERC20ExchangableBinancePeggedAsset = token => {
-  if (!token) {
-    return false;
-  }
-
-  return Object.keys(binancePeggedAssets).includes(token.address.toLowerCase());
-};
-
-const ComponentFinanceLink = ({ tokenAddress }) => (
-  <Link
-    href={getComponentFinanceLink(tokenAddress)}
-    color="blue.500"
-    isExternal
-  >
-    Component.Finance
+const XpollinateLink = () => (
+  <Link href="https://xpollinate.io/" color="blue.500" isExternal>
+    Xpollinate
   </Link>
 );
 
+const BSC_BINANCE_PEGGED_ASSETS = [
+  '0x8ac76a51cc950d9822d68b83fe1ad97b32cd580d'.toLowerCase(),
+  '0x1af3f329e8be154074d8769d1ffa4ee058b1dbc3'.toLowerCase(),
+];
+
+export const isBSCPeggedToken = token => {
+  if (!token) return false;
+  const { address, chainId } = token;
+  return (
+    chainId === 56 && BSC_BINANCE_PEGGED_ASSETS.includes(address.toLowerCase())
+  );
+};
+
+export const isGCStableToken = token => {
+  if (!token) return false;
+  const { address, chainId } = token;
+  return chainId === 100 && GC_STABLE_ASSETS.includes(address.toLowerCase());
+};
+
 export const BinancePeggedAssetWarning = ({ token, noShadow = false }) => {
   const { symbol, address, chainId } = token;
-  const { homeChainId, bridgeDirection } = useBridgeDirection();
-  const isHomeToken =
-    chainId === homeChainId &&
-    bridgeDirection === BSC_XDAI_BRIDGE &&
-    isERC20ExchangableBinancePeggedAsset(token);
+  const { getBridgeChainId, bridgeDirection } = useBridgeDirection();
+  const isGCToken =
+    bridgeDirection === BSC_XDAI_BRIDGE && isGCStableToken(token);
 
-  const binancePeggedAssetSymbol = getBinancePeggedAssetSymbol(address);
+  const isBSCToken = isBSCPeggedToken(token);
 
-  return isHomeToken ? (
-    <Flex align="center" direction="column" w="100%" mb="4">
-      <Alert
-        status="warning"
-        borderRadius={5}
-        boxShadow={noShadow ? 'none' : '0px 1rem 2rem rgba(204, 218, 238, 0.8)'}
-      >
-        <AlertIcon minWidth="20px" />
-        <Text fontSize="small">
-          Bridging {symbol} token to Binance Smart Chain DOES NOT unlock
-          Binance-Peg {binancePeggedAssetSymbol} token. If you want Binance-Peg{' '}
-          {binancePeggedAssetSymbol}, exchange
-          {symbol} on <ComponentFinanceLink tokenAddress={address} />.
-        </Text>
-      </Alert>
-    </Flex>
-  ) : null;
+  if (isGCToken) {
+    return (
+      <Flex align="center" direction="column" w="100%" mb="4">
+        <Alert
+          status="warning"
+          borderRadius={5}
+          boxShadow={
+            noShadow ? 'none' : '0px 1rem 2rem rgba(204, 218, 238, 0.8)'
+          }
+        >
+          <AlertIcon minWidth="20px" />
+          <Text fontSize="small">
+            Bridging of {symbol} tokens to Binance Smart Chain is disabled,
+            {address.toLowerCase() === DAI_TOKEN
+              ? ' bridge tokens back to the Ethereum Mainnet and '
+              : ' please '}
+            use <XpollinateLink /> bridge instead.
+          </Text>
+        </Alert>
+      </Flex>
+    );
+  }
+  if (isBSCToken) {
+    return (
+      <Flex align="center" direction="column" w="100%" mb="4">
+        <Alert
+          status="warning"
+          borderRadius={5}
+          boxShadow={
+            noShadow ? 'none' : '0px 1rem 2rem rgba(204, 218, 238, 0.8)'
+          }
+        >
+          <AlertIcon minWidth="20px" />
+          <Text fontSize="small">
+            Bridging of Binance-Peg {symbol} tokens to the{' '}
+            {getNetworkName(getBridgeChainId(chainId))} is disabled, please use{' '}
+            <XpollinateLink /> bridge instead.
+          </Text>
+        </Alert>
+      </Flex>
+    );
+  }
+  return null;
 };
--- packages/dapp/src/hooks/useTokenWarnings.jsx
+++ packages/dapp/src/hooks/useTokenWarnings.jsx
@@ -1,4 +1,8 @@
-import { BinancePeggedAssetWarning } from 'components/warnings/BinancePeggedAssetWarning';
+import {
+  BinancePeggedAssetWarning,
+  isBSCPeggedToken,
+  isGCStableToken,
+} from 'components/warnings/BinancePeggedAssetWarning';
 import { DaiWarning } from 'components/warnings/DaiWarning';
 import {
   isRebasingToken,
@@ -12,15 +16,26 @@ import {
   isDisabledStakeToken,
   StakeTokenWarning,
 } from 'components/warnings/StakeTokenWarning';
+import { BSC_XDAI_BRIDGE } from 'lib/constants';
 import React from 'react';
 
+import { useBridgeDirection } from './useBridgeDirection';
+
 export const useTokenWarnings = ({ token, noShadow = false }) => {
+  const { bridgeDirection } = useBridgeDirection();
   if (!token) return { isBridgingDisabled: false, warnings: null };
   const isTokenRebasing = isRebasingToken(token);
   const isTokenSafeMoon = isSafeMoonToken(token);
   const isTokenDisabledStake = isDisabledStakeToken(token);
+  const isTokenBSCPegged = isBSCPeggedToken(token);
+  const isTokenGCStableToBSC =
+    isGCStableToken(token) && bridgeDirection === BSC_XDAI_BRIDGE;
   const isBridgingDisabled =
-    isTokenRebasing || isTokenSafeMoon || isTokenDisabledStake;
+    isTokenRebasing ||
+    isTokenSafeMoon ||
+    isTokenDisabledStake ||
+    isTokenGCStableToBSC ||
+    isTokenBSCPegged;
   const warnings = (
     <>
       <StakeTokenWarning {...{ token, noShadow }} />
