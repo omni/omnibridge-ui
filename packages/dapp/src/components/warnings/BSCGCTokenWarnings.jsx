9fdd4855c5676a37188dd4c812b202a1d6e55730
dan13ram <dan13ram@gmail.com>
2022-05-03 23:12:49 +0530

    block sending USDT from BSC to GC #352

--- packages/dapp/src/components/warnings/BSCGCTokenWarnings.jsx
+++ packages/dapp/src/components/warnings/BSCGCTokenWarnings.jsx
@@ -19,6 +19,7 @@ const XpollinateLink = () => (
 );
 
 const BSC_BINANCE_PEGGED_ASSETS = [
+  '0x55d398326f99059ff775485246999027b3197955'.toLowerCase(), // USDT
   '0x8ac76a51cc950d9822d68b83fe1ad97b32cd580d'.toLowerCase(), // USDC
   '0x1af3f329e8be154074d8769d1ffa4ee058b1dbc3'.toLowerCase(), // DAI
 ];
